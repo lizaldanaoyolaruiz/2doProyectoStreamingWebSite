d701628e6e7e330f78dc31556cf7289720407c3d
Mauro Sebastian <sebastianseu@hotmail.com>
2026-03-16 19:15:42 -0300

    Agrego codigo al archivo home.jsx retornando dichas funciones

---
+++ src/paginas/home.jsx
@@ -0,0 +1,15 @@
+import Navbar from '../components/Navbar'
+import Destacada from '../components/Destacada'
+import RecommendedCarousel from '../components/RecommendedCarousel'
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Destacada />
+      <RecommendedCarousel />
+    </>
+  )
+}
+
+export default Home
