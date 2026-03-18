1ce981ca056318d649f15e33227ea008f8190be9
Mauro Sebastian <sebastianseu@hotmail.com>
2026-03-18 10:39:30 -0300

    agrego ruta del Home a App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import { LoginPage } from './pages/Login'
+import Home from './pages/Home'
 
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route element={<Home />} path="/"/>
+          <Route element={Home} path='./'/>
           <Route element={<LoginPage/>} path='/Login'/>
       </Routes> 
     </BrowserRouter>
